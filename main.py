b5274bac3ac0ef2d75735863b685df6d0d68d33d
Henry Ning <henryhuning@gmail.com>
2020-12-13 17:16:18 +0000

    23. [Interactive Coding Exercise] Life in Weeks 6min

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-score = 0
-height = 1.8
-isWinning = True
-print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+life_in_months = (90-int(age))*12
+life_in_weeks = (90-int(age))*52
+life_in_days = (90-int(age))*365
+
+
+print(f"You have {life_in_months} months, {life_in_weeks} weeks, {life_in_days} days left.")
+
+
+
+
+
+
+
+
